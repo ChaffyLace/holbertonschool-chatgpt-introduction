eabac332b33659a6ffa5e876b4c132a0c2441244
ChaffyLace <132900529+ChaffyLace@users.noreply.github.com>
2026-01-14 15:09:54 +0100

    Delete python-hello_world directory

--- python-hello_world/2-print.py
+++
@@ -1,3 +0,0 @@
-# chmod +x makes this file executable by adding execute permissions
-# It allows the script to be run directly from the command line
-print("Programming is like building a multilingual puzzle")
