d1985cf468d74afb37442ab2d09dd87d72b5f6ab
abdel <chaffylace@gmail.com>
2026-01-14 14:32:02 +0100

    task0

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,1 +1,3 @@
+# chmod +x makes this file executable by adding execute permissions
+# It allows the script to be run directly from the command line
 print("Programming is like building a multilingual puzzle")
